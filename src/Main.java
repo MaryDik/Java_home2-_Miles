1e3bdf071d9fee059dfefe89e4efafa18db90dc0
MaryDi <28907183@mail.ru>
2023-03-09 19:52:59 +0200

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5_000;
+        int oneMile = 20;
+        int accountBonus = ticketPrice / oneMile;
+        System.out.print("Bonus miles:" + accountBonus);
+    }
+}
